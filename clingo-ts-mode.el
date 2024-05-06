ebe2e04dd4c84ea87a3fe7675022de2b439ac51f
teeaychem <bsparkes@stanford.edu>
2024-05-06 21:33:28 +0100

    messy initial implementation of `command' choice
    made with the help of AUCTeX, to be improved…

--- clingo-ts-mode.el
+++ clingo-ts-mode.el
@@ -11,15 +11,18 @@
   :group 'languages
   :prefix "clingo-ts-")
 
+
 (defcustom clingo-ts-mode-version "0.0.1"
-  "Version of clingo-ts-mode."
+  "Version of `clingo-ts-mode'."
   :type 'string
   :group 'clingo-ts-mode)
+
 
 (defcustom clingo-ts-path (executable-find "clingo")
   "Path to clingo binary used for execution."
   :type 'string
   :group 'clingo-ts-mode)
+
 
 (defvar clingo-ts-font-lock-rules
   '(:language clingo
@@ -64,7 +67,7 @@
 
 
 (defun clingo-ts-setup ()
-  "Setup for clingo-ts-mode."
+  "Setup for `clingo-ts-mode'."
   (setq-local treesit-font-lock-settings
               (apply #'treesit-font-lock-rules
                      clingo-ts-font-lock-rules))
@@ -84,27 +87,63 @@
   (treesit-major-mode-setup))
 
 (defun run-clingo-on-current-file ()
+  "Run clingo on the file opened in the current buffer."
   (interactive)
   (run-clingo (buffer-file-name) nil))
 
-(defun run-clingo (file args)
 
+(defun run-clingo (file args)
+  "Run clingo on FILE with ARGS."
   (let ((clingo-buffer (get-buffer-create "* clingo *")))
     (with-current-buffer clingo-buffer
-      (call-process "clingo" file clingo-buffer nil "-n0")
+      (call-process "clingo" file clingo-buffer nil args)
       (insert "\n --- \n")
-      (goto-char (point-max))
-      )
-    (display-buffer clingo-buffer)
-    )
+      (goto-char (point-max)))
+    (display-buffer clingo-buffer)))
 
-  )
 
 (defun run-clingo-on-current-region (start end)
+  "Run clingo on the region from START to END."
   (interactive "r")
   (let ((temp-file (make-temp-file "clingo-region" nil ".lp" nil)))
     (write-region start end temp-file t)
     (run-clingo temp-file nil)))
+
+
+(defgroup clingo-command nil
+  "Commands used by `clingo-ts-mode'."
+  :group 'clingo-ts-mode)
+
+;; (string-join args " ")
+
+(defcustom clingo-command-list
+  '(("clingo" "")
+    ("subset minimal" "--models=0 --enum-mode=domRec --heuristic=Domain --dom-mod=5,16"))
+  "This is where the comment goes."
+  :group 'clingo-command
+  :type '(repeat (group (string :tag "Name") (string :tag "Commands"))))
+
+
+
+(defun clingo-command-query ()
+  "Uh."
+  (let* ((default "clingo")
+         (completion-ignore-case t)
+         (answer (completing-read
+                  (concat "Command (default " default "): ")
+                  clingo-command-list nil t
+                  nil nil default)))
+    (if (and answer
+             (not (string-equal answer "")))
+        (car-safe (cdr-safe (assoc-string answer clingo-command-list)))
+      (car-safe (cdr-safe (assoc-string default clingo-command-list))))))
+
+
+(defun clingo-command-main ()
+  "Ah heck."
+  (interactive)
+  (let ((option (clingo-command-query)))
+    (run-clingo (buffer-file-name) option)))
 
 
 ;;; define clingo-ts-mode
@@ -117,6 +156,7 @@
 
 (define-key clingo-ts-mode-map (kbd "C-c C-c C-f") #'run-clingo-on-current-file)
 (define-key clingo-ts-mode-map (kbd "C-c C-c C-r") #'run-clingo-on-current-region)
+(define-key clingo-ts-mode-map (kbd "C-c C-c C-c") #'clingo-command-main)
 
 (provide 'clingo-ts-mode)
 
